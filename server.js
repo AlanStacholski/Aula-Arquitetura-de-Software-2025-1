6aa6c80f5f83087aa63b1083efa76782ca9009f9
Alan Stacholski Júnior <122293683+AlanStacholski@users.noreply.github.com>
2025-02-24 21:40:16 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+//import http from "http";
+import app from "./src/app.js";
+
+const PORT = 3000;
+const rotas =  {
+    "/":"API com Node e Express.js",
+    "/posts": "Rota de postagens",
+    "/autores": "Rota de autores",
+}
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain"});
+//     res.end(rotas[req.url]);
+// });
+
+app.listen(PORT, () => {
+    console.log("Servidor na Escuta!");
+});
