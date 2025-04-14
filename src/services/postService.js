126590b5571c4f9a55ff4c5d1ac7aeebc7b9d8af
Alan Stacholski <alamstacholski@gmail.com>
2025-04-14 20:25:36 -0300

    Primeiro commit aula 14/04/2025

---
+++ src/services/postService.js
@@ -0,0 +1,50 @@
+import { PostRepository } from "../repositories/postRepository.js";
+import { PostDto } from "../dtos/postDto.js";
+import { author } from "../models/Author.js";
+
+
+export class PostService {
+    postRepository = new PostRepository();
+    getAllPosts = async () => {
+        return await this.postRepository.findAll();
+    }
+    createPost = async (postData) => {
+        const foundAuthor = await author.findById(postData.author);
+        if (!foundAuthor) {
+            throw new Error("Author não encontrado!");
+        }
+        const completePost = {
+            ...PostDto.fromRequest(postData),
+            author: { ...foundAuthor._doc },
+        }
+        return await this.postRepository.create(completePost);
+    }
+    getPostById = async (id) => {
+        const post = await this.postRepository.findById(id);
+        if (!post) {
+            throw new Error("Post não encontrado!");
+        }
+        return post;
+    }
+    
+    // Iniciando a partir daqui...
+
+    updatePost = async (id, postData) => {
+        let updateData = {...postData};
+
+        if (postData.author) {
+            const foundAuthor = await author.findById(postData.author);
+            if (!foundAuthor) {
+                throw new Error("Author não encontrado");
+            }
+            updateData.author = {...foundAuthor._doc};
+        }
+
+        const updatePost = this.postRepository.update(id, updateData);
+        if (!updatePost) {
+            throw new Error("Post não encontrado!");
+        }
+        return updatePost;
+    }
+
+}
